cd4a10a9146ce00e8f0352df2fda8214485a85d0
glen-no <gagolovin1205@gmail.com>
2024-12-27 21:02:06 +0300

    Add files via upload

--- path_of_story.py
+++ path_of_story.py
@@ -4,7 +4,7 @@ info_player = {'hp':1000,'lvl':1,'сила печени': 5,'exp':0,'atack':5,'s
 player_fight = {}
 enemies = ([{'name':'Дядя_вася' ,'hp':100,'script':'Слабоват ты молокосос','scriptlose':'ЙК нало бутеть как то повторить','count':10}
             ,{'name':'Трудовик', 'hp':250,'script':'Слабовата твоя печень','scriptlose':'Ух неплохо покутили','count':10}
-            ,{'name':'Отец','hp':1000,'script':'Тебе меня никогда не препить','scriptlose':' Йк Невозможно Йк','count':10}])
+            ,{'name':'Отец','hp':1000,'script':'Тебе меня никогда не перепить','scriptlose':' Йк Невозможно Йк','count':10}])
 drinks = [{'name':'Пиво','hp':50},{'name':'Джин','hp':100},{'name':'Водка','hp':200}]
 snacks = [{'name':'чипсы','health':10},
           {'name':'сухарики','health':10},
@@ -26,6 +26,7 @@ def play():
     n1 = random.randint(0, len(drinks)-1)
     n2 = random.randint(0, len(snacks)-1)
     n3 = random.randint(0, len(fight_enemies)-1)
+    fun = random.choice(['pain','food'])
     info_enemy = (enemies[n])
     info_drink = (drinks[n1])
     fight = (fight_enemies[n3])
@@ -54,14 +55,30 @@ def play():
         Ваше HP-({info_player["hp"]}),HP противника-({info_enemy["hp"]})''')
         sleep(0.5)
         print()
-    if (info_enemy['hp']<=0):
+    if (info_enemy['hp']<=info_player['hp']):
         print(f'''            you win
      {info_enemy["name"]}-({info_enemy["scriptlose"]})''')
         info_player['exp']+=1
         info_enemy['count']-=1
-    
-    if (info_player['hp']<=0):
+    else:
         print(f'''           you lose
      {info_enemy["name"]}-({info_enemy["script"]})''')
+        
+    if event <=5:
+     print('''СЛУЧАЙНОЕ СОБЫТИЕ ''')
+     if fun =='food':
+      print(f'you ate {snack["name"]}')
+      info_player['hp'] += snack['health']
+     elif fun=='pain':
+      print('На вас напали')
+      while info_player['hp'] and fight['hp']>=0:
+         print(f'ваше здоровье-{info_player["hp"]},здоровье({fight["name"]})-{fight["hp"]}')
+         info_player['hp']-= (fight['atack']*fight['strench'])-info_player['armor']*0.1
+         fight['hp']-= (info_player['atack']*info_player['strench'])-fight['armor']*0.1
+         if info_player['hp']<=0:
+           print('you lose')
+         elif fight['hp']<=0:
+          print('you win')
+
    
-    print(info_player['exp'],info_player['lvl'])
+    
--- story.py
+++ story.py
@@ -7,13 +7,31 @@ n = 1
 
 while n==1:
       
+       print(NAME)
        print(f'ваше здоровье-{info_player["hp"]}')
-       choise2= input('''sleep-1 or driking-2: ''')
+       print(f'опыт:{info_player["exp"]}/{max["max"]} уровень-{info_player["lvl"]}')
+      
+       choise2= input('''
+sleep-1 
+driking-2
+gym-3
+inventory-4
+menu-5: ''')
        if choise2=='1':
             print('you sleep and feels a little healthy')
+            print()
             info_player['hp']= 1000
        elif choise2=='2':
             play()
+       elif choise2== '3':
+             print('biba') 
+             sleep(10)   
+             print()
+       elif choise2=='4':
+             print('biba')  
+             sleep(10) 
+             print()       
+       elif choise2=='5':     
             choise = input('''            хотите продолжить играть? 
             (да=Y нет=N):  ''')
             if choise=='Y':
